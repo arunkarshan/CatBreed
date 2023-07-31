d7795ec9139e6605d74ac1fad0d1d37dc48c1200
Arun Shankar <arunkarshan@gmail.com>
2023-07-31 15:16:24 +0100

    HomeTask with the suggested Improvements
    HomeTask with the suggested Improvements

--- app/src/main/java/com/example/codereviewtask_jc51/ApplicationDependencies.kt
+++
@@ -1,9 +0,0 @@
-package com.example.codereviewtask_jc51
-
-import com.example.codereviewtask_jc51.data.CatRepository
-import com.example.codereviewtask_jc51.data.FavoriteCatSaver
-
-object ApplicationDependencies {
-    lateinit var catRepository: CatRepository
-    lateinit var favoriteCatSaver: FavoriteCatSaver
-}
